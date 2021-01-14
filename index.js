660b2ef76f5cd8d996b6929f15c9f8e3893eaa2f
秦秋旭 <qiuxu.qin@outlook.com>
2021-01-15 00:21:10 +0800

    8.5: Books by genre
    修改查询 allBooks，以便用户可以给出一个可选参数genre。 回应应该只包括这种类型的书籍。
    当给定两个可选参数时，查询必须正常工作:

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -112,10 +112,14 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
-        return books;
+      let result = books;
+      if (args.author) {
+        result = result.filter(book => book.author === args.author);
       }
-      return books.filter(book => book.author === args.author)
+      if (args.genre) {
+        result = result.filter(book => book.genres.includes(args.genre));
+      }
+      return result;
     },
     allAuthors: () => authors
   },
