d81d074007b08a69fbc6c42c38d2df9610d530ac
秦秋旭 <trolxyqin@outlook.com>
2021-01-14 18:52:13 +0800

    8.2: All books
    实现查询 allBooks，它返回所有图书的详细信息。

--- index.js
+++ index.js
@@ -84,9 +84,18 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Book {
+    id: ID!
+    title: String!
+    author: String!
+    published: Int!
+    genres: [String!]!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
@@ -94,6 +103,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
